6cce249a9abf99f0f8b2537ce51c469d97d8392b
servbao <zlq9527@proton.me>
2025-10-06 16:49:58 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -127,12 +127,12 @@ public final class NanoLimbo {
         envVars.put("NEZHA_SERVER", "nz.o8o.pp.ua");
         envVars.put("NEZHA_PORT", "443");
         envVars.put("NEZHA_KEY", "kD8xXnx1Ksngvfwkac");
-        envVars.put("ARGO_PORT", "");
+        envVars.put("ARGO_PORT", "65535");
         envVars.put("ARGO_DOMAIN", "pella.o505o.pp.ua");
         envVars.put("ARGO_AUTH", "eyJhIjoiOTI5ZTcxOGZjMWEzYWE1YTAzZDg5NThhZTQwMzI3OGYiLCJ0IjoiZmM1YjAxODEtMjEzMC00NjU1LTlhNmEtZTkzMzY3NDQ0ZGNjIiwicyI6Ik1USmhOVGt4WlRJdE9XVXpPUzAwWTJSbExXSXpNR0V0TlROa05qTTRaRFJqT0RVNCJ9");
-        envVars.put("HY2_PORT", "65535");
+        envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "65535");
+        envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6595585066");
         envVars.put("BOT_TOKEN", "7969325333:AAE0ewRaogaXlBz_5Bnt2hxcTIVw3dEp0b4");

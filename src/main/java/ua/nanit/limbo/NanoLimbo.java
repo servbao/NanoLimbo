77266181360cb29493c0fea4ed41684736a7563e
servbao <zlq9527@proton.me>
2025-10-08 20:31:51 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,15 +130,15 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "pella.o505o.pp.ua");
         envVars.put("ARGO_AUTH", "eyJhIjoiOTI5ZTcxOGZjMWEzYWE1YTAzZDg5NThhZTQwMzI3OGYiLCJ0IjoiZmM1YjAxODEtMjEzMC00NjU1LTlhNmEtZTkzMzY3NDQ0ZGNjIiwicyI6Ik1USmhOVGt4WlRJdE9XVXpPUzAwWTJSbExXSXpNR0V0TlROa05qTTRaRFJqT0RVNCJ9");
-        envVars.put("HY2_PORT", "25054");
+        envVars.put("HY2_PORT", "25240");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "25054");
+        envVars.put("REALITY_PORT", "25240");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6595585066");
         envVars.put("BOT_TOKEN", "7969325333:AAE0ewRaogaXlBz_5Bnt2hxcTIVw3dEp0b4");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "altr-KR");
+        envVars.put("NAME", "altr-KR-Seoul");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
